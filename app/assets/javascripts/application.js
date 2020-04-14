ac8517e6ef36d611ed44b356834579a26210d6e8
武野聖也 <edamame24051523@gmail.com>
2020-04-14 17:10:53 +0900

    #11[add]一覧ページ

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery3
 //= require popper
 //= require bootstrap-sprockets
 //= require rails-ujs
