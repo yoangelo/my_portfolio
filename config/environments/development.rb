ae7659ecffc1f8e86e674fc857f351db8675c274
武野聖也 <edamame24051523@gmail.com>
2020-04-17 00:56:11 +0900

    #14[add]メール認証機能

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,7 +31,18 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :user_name => "edamame24051523@gmail.com",
+    :password => "bhxyovqohijpzyvs",
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 
   config.action_mailer.perform_caching = false
 
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -18,7 +18,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  config.mailer_sender = 'account_activation0328@gmail.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
@@ -296,6 +296,6 @@ Devise.setup do |config|
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
-  config.omniauth :twitter, "rTADHVPrvGu74ligSV1uhMy3P", 
+  config.omniauth :twitter, "rTADHVPrvGu74ligSV1uhMy3P",
   "kpGcSOrK4ZXU9IOkTJVZwRwZCVxBfjId5pS9Vk5GnRb7owviWl"
 end
