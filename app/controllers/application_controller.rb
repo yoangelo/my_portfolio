961dcde59466c4ae4fb6aa717b1508472f012242
武野聖也 <edamame24051523@gmail.com>
2020-04-16 20:53:49 +0900

    #14[add]deviseによるTwitterOAuth認証機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path_for(resource)
+    reviews_path
+  end
+
+  protected
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+    end
+
+  private
+    def sign_in_required
+      redirect_to new_user_session_url unless user_signed_in?
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { :omniauth_callbacks => "omniauth_callbacks" }
   root 'static_pages#home'
   resources :reviews
-  
+
 end
