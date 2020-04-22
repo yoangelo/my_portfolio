1e3763bcccc6f241c992f44cd1ee98505983e9d3
武野聖也 <edamame24051523@gmail.com>
2020-04-22 22:37:42 +0900

    #23[add]Likeコントローラ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  get 'likes/create'
+  get 'likes/destroy'
   devise_for :users, controllers: {
     :omniauth_callbacks => "omniauth_callbacks",
     :registrations => 'users/registrations'
   }
   root 'static_pages#home'
   resources :reviews
+  resources :likes, only: [:create, :destroy]
 
 end
