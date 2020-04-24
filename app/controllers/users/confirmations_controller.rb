39a28cd3e085abeb13ab1e9dc35672f6f92dc9c2
武野聖也 <edamame24051523@gmail.com>
2020-04-24 18:04:09 +0900

    #14[fix]メール認証後のログイン機能解除(未)

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -24,7 +24,9 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
   # end
 
   # The path used after confirmation.
-  # def after_confirmation_path_for(resource_name, resource)
-  #   super(resource_name, resource)
-  # end
+  private
+  def after_confirmation_path_for(resource_name, resource)
+    sign_in(resource)
+    reviews_path
+  end
 end
