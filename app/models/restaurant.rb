04c7e91342c79da58f2d33bc28f7775ab5145c0d
武野聖也 <edamame24051523@gmail.com>
2020-05-04 16:06:36 +0900

    #4[fix]unicorn起動時のエラー修正

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,7 +2,6 @@ class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
   validates :name, presence: true
   validates :address, presence: true
-  va
 
   def serch_equal_restaurant
 
