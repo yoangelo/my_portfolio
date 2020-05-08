1459910b0c73e3424a9363bcfddf784465a724c4
武野聖也 <edamame24051523@gmail.com>
2020-05-09 06:42:37 +0900

    #40[update]restaurantのネストより正常な動作になるように修正

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -9,7 +9,21 @@ class RestaurantsController < ApplicationController
 
 
   def create
-
+    puts params[:name]
+    @restaurant = Restaurant.new(
+      name:    params[:name],
+      address: params[:address],
+      res_id:  params[:res_id],
+      tell:    params[:tell]
+    )
+    if @restaurant.save
+      puts "保存されました"
+      redirect_to new_restaurant_review_path(restaurant_id: @restaurant.id)
+    else
+      puts "すでに保存されてます"
+      @rest = Restaurant.find_by(res_id: @restaurant.res_id)
+      redirect_to new_restaurant_review_path(restaurant_id: @rest.id)
+    end
   end
-  
+
 end
