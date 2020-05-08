19c4417fc926c9305068065556ce847d43727f90
武野聖也 <edamame24051523@gmail.com>
2020-05-09 06:48:19 +0900

    #40[update]restaurantを降順にした

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,6 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
+  default_scope -> { order(created_at: :desc) }
   validates :name, presence: true
   validates :address, presence: true
   validates :res_id, uniqueness: true
