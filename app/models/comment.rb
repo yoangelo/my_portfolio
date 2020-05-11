64f9d369330bfae27c6b0e8e6720dd59a34399d3
武野聖也 <edamame24051523@gmail.com>
2020-05-11 23:37:59 +0900

    #43[update]presenceとlengthのバリデーションを追加

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :review
   has_many :notifications, dependent: :destroy
+  validates :body, presence: true, length: { maximum: 200 }
 end
