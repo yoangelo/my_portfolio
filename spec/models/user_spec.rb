b8f295563244a4c12a7d6bb3319d9bb82f60e554
武野聖也 <edamame24051523@gmail.com>
2020-05-17 04:09:45 +0900

    [update]User model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,32 +1,109 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "Eメールアドレス、パスワードがあれば有効な状態であること" do
+
+  it "有効なファクトリを持つこと" do
     expect(FactoryBot.build(:user)).to be_valid
   end
 
-  it "メールアドレスがなければ無効な状態であること" do
-    user = FactoryBot.build(:user, email: nil)
-    user.valid?
-    expect(user.errors[:email]).to include("が入力されていません。")
+  context "Userモデルのバリデーション" do
+
+    it "メールアドレスがなければ無効な状態であること" do
+      user = FactoryBot.build(:user, email: nil)
+      user.valid?
+      expect(user.errors[:email]).to include("が入力されていません。")
+    end
+
+    it "パスワードがなければ無効な状態であること" do
+      user = FactoryBot.build(:user, password: nil)
+      user.valid?
+      expect(user.errors[:password]).to include("が入力されていません。")
+    end
+
+    it "重複したメールアドレスなら無効な状態であること" do
+      FactoryBot.create(:user)
+      user2 = FactoryBot.build(:user, email: "test@test")
+      user2.valid?
+      expect(user2.errors[:email]).to include("は既に使用されています。")
+    end
   end
 
-  it "重複したメールアドレスなら無効な状態であること" do
-    FactoryBot.create(:user, email: "aaron@example.com")
-    user = FactoryBot.build(:user, email: "aaron@example.com")
-    user.valid?
-    expect(user.errors[:email]).to include("は既に使用されています。")
-  end
 
-  it "ユーザー一人につき一つのプロフィールを持ち、ユーザーが削除されたら同時に削除されること" do
-    is_expected.to have_one(:profile).dependent(:destroy)
-  end
+  describe "各モデルとのアソシエーション" do
+    before do
+      @user = FactoryBot.create(:user)
+      rest = Restaurant.create(name: "test_restaurant", address: "1-1-1", res_id: "1111111")
+      prof = Profile.create(user_id: @user.id, name: "TEST")
+      rev  = Review.create(user_id: @user.id, restaurant_id: rest.id, title: "Test_Title", body: "test_body")
+      Like.create(review_id: rev.id, user_id: @user.id)
+      Comment.create(review_id: rev.id, user_id: @user.id, body: "TEST")
+    end
+    let(:association) do
+      described_class.reflect_on_association(target)
+    end
 
-  it "ユーザーは多数のレビューを持ち、ユーザーが削除されたら同時に削除されること"
-  it "ユーザーは多数のいいねを持ち、ユーザーが削除されたら同時に削除されること"
-  it "ユーザーは多数のコメントを持ち、ユーザーが削除されたら同時に削除されること"
-  it "ユーザーは多数の通知発信を持ち、ユーザーが削除されたら同時に削除されること"
-  it "ユーザーは多数の通知受信を持ち、ユーザーが削除されたら同時に削除されること"
-  it "Twitter認証時、uidとproviderとusernameのレコードを作成すること"
-  it "Twitterでの認証後サインアップするとユーザー登録され、以降はTwitter認証だけでログインできること"
+    context "Profileモデルとのアソシエーション" do
+      let (:target) { :profile }
+
+      it "Profileとの関連付けはhas_oneであること" do
+        expect(association.macro).to eq :has_one
+      end
+
+      it "Userが削除されたらProfileも削除されること" do
+        expect{ @user.destroy }.to change { Profile.count }.by(-1)
+      end
+    end
+
+    context "Reviewモデルとのアソシエーション" do
+      let (:target) { :review }
+
+      it "Reviewとの関連付けはhas_manyであること" do
+        expect(association.macro).to eq :has_many
+      end
+
+      it "Userが削除されたらReviewも削除されること" do
+        expect{ @user.destroy }.to change { Review.count }.by(-1)
+      end
+    end
+
+    context "Likeモデルとのアソシエーション" do
+      let (:target) { :likes }
+
+      it "Likeとの関連付けはhas_manyであること" do
+        expect(association.macro).to eq :has_many
+      end
+
+      it "Userが削除されたらLikeも削除されること" do
+        expect{ @user.destroy }.to change { Like.count }.by(-1)
+      end
+    end
+
+    context "Commentモデルとのアソシエーション" do
+      let (:target) { :comments }
+
+      it "Commentとの関連付けはhas_manyであること" do
+        expect(association.macro).to eq :has_many
+      end
+
+      it "Userが削除されたらCommentも削除されること" do
+        expect{ @user.destroy }.to change { Comment.count }.by(-1)
+      end
+    end
+
+    context "active_notificationsとのアソシエーション" do
+      let (:target) { :active_notifications }
+
+      it "Reviewとの関連付けはhas_manyであること" do
+        expect(association.macro).to eq :has_many
+      end
+    end
+
+    context "passive_notificationsとのアソシエーション" do
+      let (:target) { :passive_notifications }
+
+      it "Reviewとの関連付けはhas_manyであること" do
+        expect(association.macro).to eq :has_many
+      end
+    end
+  end
 end
