a56791339adabee7adbc86edd5ca864071bd26bc
武野聖也 <edamame24051523@gmail.com>
2020-05-18 01:09:05 +0900

    [add]model spec

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Profile, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "有効なファクトリを持つこと" do
+    expect(FactoryBot.build(:profile)).to be_valid
+  end
+
+  context "Profileモデルのバリデーション" do
+
+    it "名前がなければ無効な状態であること" do
+      profile = FactoryBot.build(:profile, name: nil)
+      profile.valid?
+      expect(profile.errors[:name]).to include("を入力してください")
+    end
+
+  end
+  describe "各モデルとのアソシエーション" do
+    let(:association) do
+      described_class.reflect_on_association(target)
+    end
+    context "Userモデルとのアソシエーション" do
+      let (:target) { :user }
+
+      it "Userとの関連付けはbelongs_toであること" do
+        expect(association.macro).to eq :belongs_to
+      end
+    end
+  end
 end
