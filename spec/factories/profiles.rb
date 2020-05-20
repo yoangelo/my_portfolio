3df24d4bdbc0fb5f75ee149a6a2f2f5ab47fae30
武野聖也 <edamame24051523@gmail.com>
2020-05-20 17:37:42 +0900

    [後ほど修正]

--- spec/factories/profiles.rb
+++ spec/factories/profiles.rb
@@ -4,4 +4,10 @@ FactoryBot.define do
     name { "TEST" }
     association :user
   end
+
+  factory :profile_user, parent: :user do
+    user_id { 1 }
+    name { "TEST" }
+    association :user
+  end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,11 @@ FactoryBot.define do
   factory :user do
     email {"test@test"}
     password {"password"}
+    id {1}
+    # trait :user_has_profile do
+    #   after(:build) do |user|
+    #     user.profile << FactoryBot.build(:profile_user)
+    #   end
+    # end
   end
 end
