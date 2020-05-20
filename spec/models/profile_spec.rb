f052d42df57068738a28492a8b6ff9d8c113b8aa
武野聖也 <edamame24051523@gmail.com>
2020-05-20 17:38:02 +0900

    [後ほど修正]

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Profile, type: :model do
   it "有効なファクトリを持つこと" do
-    expect(FactoryBot.build(:profile)).to be_valid
+    expect(FactoryBot.build(:profile_user)).to be_valid
   end
 
   context "Profileモデルのバリデーション" do
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
 
   it "有効なファクトリを持つこと" do
-    expect(FactoryBot.build(:user)).to be_valid
+    expect(FactoryBot.create(:user, :user_has_profile)).to be_valid
   end
 
   context "Userモデルのバリデーション" do
