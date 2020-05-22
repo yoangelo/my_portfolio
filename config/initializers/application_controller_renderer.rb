7338f1507de3210066dbfe67764881ab1b20e039
武野聖也 <edamame24051523@gmail.com>
2020-05-23 00:08:07 +0900

    [update]Refile.secret_keyの暗号化

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -7,4 +7,4 @@
 #   )
 # end
 
-Refile.secret_key = '2b703405d3746898529461ffde0ec6b93e0190d8ce8f446d3da8f7fd1b280141a4fe0276dbd93c740782a2b9bf6065ebbe190cf788096fe218ed055aaedea9fa'
+Refile.secret_key = ENV["REFILE.SECRET_KEY"]
