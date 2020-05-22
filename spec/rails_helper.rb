08470fe2423f67538821ca86c5fbe3f16ce58d3a
武野聖也 <edamame24051523@gmail.com>
2020-05-23 04:53:22 +0900

    [update]JavaScriptを読み込めるようにした

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -85,4 +85,8 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  
+  config.include FactoryBot::Syntax::Methods
+  # ajaxの処理完了を待つためのヘルパーメソッド
+  config.include WaitForAjax, type: :system
 end
