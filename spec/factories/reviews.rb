d61d775362c4381259181adfb4dfe46c932a031b
武野聖也 <edamame24051523@gmail.com>
2020-05-27 05:51:47 +0900

    [update]search system specの実施に伴うreview_botの追加

--- spec/factories/reviews.rb
+++ spec/factories/reviews.rb
@@ -1,7 +1,19 @@
 FactoryBot.define do
-  factory :review do
+  factory :review, class:Review do
     title { "TEST" }
     body { "test_body" }
+    association :restaurant
+    association :user
+  end
+  factory :search_a, class:Review do
+    title { "foo" }
+    body { "bar" }
+    association :restaurant
+    association :user
+  end
+  factory :search_b, class:Review do
+    title { "hoge" }
+    body { "piyo" }
     association :restaurant
     association :user
   end
