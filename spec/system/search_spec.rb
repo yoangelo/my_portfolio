32d1eaa797427c6bf345e440db6ae82928822bbf
武野聖也 <edamame24051523@gmail.com>
2020-05-30 16:38:53 +0900

    [clean]rubocop

--- spec/system/search_spec.rb
+++ spec/system/search_spec.rb
@@ -1,8 +1,9 @@
 require "rails_helper"
 
 RSpec.describe "Searchs", type: :system do
-  let (:current_user) { FactoryBot.create(:user) }
-  let (:current_rest) { FactoryBot.create(:restaurant) }
+  let(:current_user) { FactoryBot.create(:user) }
+  let(:current_rest) { FactoryBot.create(:restaurant) }
+
   describe "検索機能" do
     before do
       FactoryBot.create(:search_a, restaurant_id: current_rest.id, user_id: current_user.id)
@@ -13,7 +14,7 @@ RSpec.describe "Searchs", type: :system do
     it "検索したキーワード「hoge」が本文中に含まれる記事が一覧表示される" do
       fill_in "search", with: "piyo"
       click_button "検索"
-      expect(page).to_not have_content "foo"
+      expect(page).not_to have_content "foo"
       expect(page).to have_content "hoge"
       click_on "hoge"
       expect(page).to have_content "piyo"
