43ee0b2f5f13159ad7dcae0490f93814e97dba46
武野聖也 <edamame24051523@gmail.com>
2020-06-03 01:53:53 +0900

    [検証]元に戻す,失敗箇所にpage.html挟む

--- spec/system/restaurant_spec.rb
+++ spec/system/restaurant_spec.rb
@@ -21,23 +21,30 @@ RSpec.describe "Restaurants", type: :system do
       end
 
       it "検索したキーワードと一致した店が一覧表示され、投稿ページにアクセスできること" do
-        # fill_in "name", with: "マクドナルド"
-        # find("input", id: "rest_search").click
-        expect(page).to have_selector 'li', text: "マクドナルド"
-        # within first("li", id: "rest_list") do
-        #   find("input").choose
-        # end
+        fill_in "name", with: "マクドナルド"
+        find("input", id: "rest_search").click
+        page.html
+        within first("ul", id: "rest_lists") do
+          expect(page).to have_selector 'li', text: "マクドナルド"
+        end
+        within first("li", id: "rest_list") do
+          find("input").choose
+        end
+        click_button "登録する"
         # expect { click_button "登録する" }.to change(Restaurant, :count).by(1)
-        # expect(page).to have_selector 'h1', text: "口コミを投稿する"
+        expect(page).to have_selector 'h1', text: "口コミを投稿する"
       end
 
-      # it "文字を入力しなおすと、一覧表示された店がクリアされること" do
-      #   fill_in "name", with: "マクドナルド"
-      #   find("input", id: "rest_search").click
-      #   expect(page).to have_selector 'li', text: "マクドナルド"
-      #   fill_in "name", with: ""
-      #   expect(page).not_to have_selector 'li', text: "マクドナルド"
-      # end
+      it "文字を入力しなおすと、一覧表示された店がクリアされること" do
+        fill_in "name", with: "マクドナルド"
+        find("input", id: "rest_search").click
+        page.html
+        within first("ul", id: "rest_lists") do
+          expect(page).to have_selector 'li', text: "マクドナルド"
+        end
+        fill_in "name", with: ""
+        expect(page).not_to have_selector 'li', text: "マクドナルド"
+      end
     end
   end
 
