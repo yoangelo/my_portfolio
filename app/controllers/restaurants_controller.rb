6b9b8fccd8ad029a773f0f984d6a2e951cf10a3b
武野聖也 <edamame24051523@gmail.com>
2020-06-03 02:49:07 +0900

    [test]

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,6 +4,7 @@ class RestaurantsController < ApplicationController
 
   def new
     @restaurant = Restaurant.new
+    @user = User.last
     respond_to do |format|
       format.html
       format.json
