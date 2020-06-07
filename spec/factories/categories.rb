e637573fc73d5aa3be1e10f7e8ed13ffbcdce2ca
武野聖也 <edamame24051523@gmail.com>
2020-06-07 23:28:41 +0900

    #75[update]category_FactoryBotの作成方法を変更

--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -1,15 +1,5 @@
 FactoryBot.define do
-  sequence :category_name do |i|
-    "カテゴリその#{i}"
-  end
-  sequence :category_id do |i|
-    i
-  end
   factory :category do
     name { "カテゴリ" }
-  end
-  factory :some_category, class: "Category" do
-    name { generate :category_name }
-    id { generate :category_id }
   end
 end
