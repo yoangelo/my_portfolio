51e8d38df4f61c04684cb85ba533407dd1a5705d
武野聖也 <edamame24051523@gmail.com>
2020-06-12 02:38:37 +0900

    [add]subgenreカラム追加#81

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_11_165228) do
+ActiveRecord::Schema.define(version: 2020_06_11_173642) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -99,6 +99,7 @@ ActiveRecord::Schema.define(version: 2020_06_11_165228) do
     t.text "image_url_1"
     t.text "image_url_2"
     t.string "prefecture"
+    t.string "subgenre"
   end
 
   create_table "review_category_relations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
